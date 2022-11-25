c438f9211df88c110b72d4e3308aa9c46198b395
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-11-25 20:02:36 +0545

    first render

--- basic scene/script.js
+++ basic scene/script.js
@@ -1,5 +1,25 @@
+const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+//sizes
+const sizes = {
+  width: 600,
+  height: 400,
+};
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 4;
+camera.position.x = 2;
+camera.position.y = 2;
+scene.add(camera);
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
