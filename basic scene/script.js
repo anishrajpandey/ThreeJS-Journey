26e4c80b260f4e149bc2b3cc03ef6ddc00a5e400
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-11-25 19:45:52 +0545

    basic scene

---
+++ basic scene/script.js
@@ -0,0 +1,5 @@
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
