5e7584d6cbea96fe83f58b6f2a4c43f3562fcf0b
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-12-03 22:20:52 +0545

    commit

--- basics/src/script.js
+++ basics/src/script.js
@@ -1,23 +1,23 @@
 import * as THREE from "three";
 const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const mesh = new THREE.Mesh(geometry, material);
 
 // mesh.position.x = 0;
 // mesh.position.y = 0;
 // mesh.position.z = 0;
 // mesh.position.normalize();
-mesh.position.set(0.9, -0.6, 1);
-mesh.scale.set(2, 1, 0.5);
+// mesh.position.set(0.9, -0.6, 1);
+// mesh.scale.set(2, 1, 0.5);
 // mesh.rotation.z = Math.PI * 0.25;
 // mesh.rotation.x = 1;
 // mesh.rotation.y = 0.92;
 // mesh.rotation.x = 0.3;
 // mesh.rotation.z = 0.4;
-mesh.quaternion.x = 0.3;
-mesh.quaternion.z = 0.4;
+// mesh.quaternion.x = 0.3;
+// mesh.quaternion.z = 0.4;
 // mesh.quaternion.x = 0.4;
 //todo scene.add(mesh);
 // console.log(mesh.position.length());
@@ -36,38 +36,43 @@ camera.position.y = 0.9;
 
 scene.add(camera);
 
-console.log(
-  "🤔 > mesh.position.distanceTo(camera.position);",
-  mesh.position.distanceTo(camera.position)
-);
+// console.log(
+//   "🤔 > mesh.position.distanceTo(camera.position);",
+//   mesh.position.distanceTo(camera.position)
+// );
 
 // axes helper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 //renderer
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 
 //group
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "red" })
-);
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "blue" })
-);
-cube2.position.x = -2;
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "green" })
-);
-cube3.position.x = 3;
+// const cube1 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "red" })
+// );
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "blue" })
+// );
+// cube2.position.x = -2;
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "green" })
+// );
+// cube3.position.x = 3;
 
-const group = new THREE.Group();
-group.add(cube1, cube2, cube3);
+// const group = new THREE.Group();
+// group.add(cube1, cube2, cube3);
 // group.add(mesh);
-scene.add(group);
+// scene.add(group);
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+const cubeGeometry = new THREE.BoxGeometry(2, 2);
+const basicMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const boxMesh = new THREE.Mesh(cubeGeometry, basicMaterial);
+scene.add(boxMesh);
