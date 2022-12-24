27d8021e489fd65f0c4d31903b7747afc3684933
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-12-24 20:59:42 +0545

    transform textures

--- basics/src/script.js
+++ basics/src/script.js
@@ -151,6 +151,12 @@ const loadingManager = new THREE.LoadingManager();
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
 const texture = textureLoader.load("assets/images/b507675cedd1f96a.png");
+texture.repeat.x = 1;
+texture.repeat.y = 1;
+// texture.wrapT = THREE.MirroredRepeatWrapping;
+// texture.wrapS = THREE.MirroredRepeatWrapping;
+// texture.offset.y = 3;
+texture.rotation = Math.PI * 0.25;
 loadingManager.onStart = () => {
   console.log("start");
 };
